5ce22704442e03b0a4a53d32553a70146531e5eb
“[Khushi <khushivaish16@gmail.com>
2021-07-12 09:38:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var table = document.getElementById("tableid");
+if(table !=null)
+{
+    for(var i=0 ; i<table.rows.length; i++)
+    {
+        for(var j=0 ; j<table.rows[i].cells.length ; j++)
+        table.rows[i].cells[j].onclick=
+        function()
+        {
+            if(this.style.backgroundColor == "red")
+            this.style.backgroundColor == "white";
+            else
+            this.style.backgroundColor == "red";
+        };
+    }
+}
